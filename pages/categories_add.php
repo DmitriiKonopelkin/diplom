f5db14931b11a8f893e0c75183467507c8cf6597
DmitriiKonopelkin <dm.konopelkin@gmail.com>
2025-05-06 12:24:29 +0300

    add categories

---
+++ pages/categories_add.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Добавление категорий</title>
+    <link rel='stylesheet' href='../css/style.css'/>
+</head>
+<body>
+<?php  include "../includes/header.php"; ?>
+<main class='container-fluid'>
+    <form action='#' method='post'>
+        <div>
+            <input type='text' name='name' placeholder='Название категории'/>
+        </div>
+        <div>
+            <textarea name='description'>Описание категории</textarea>
+        </div>
+        <div>
+            <input type='submit' value='Добавить категорию'/>
+        </div>
+    </form>
+</main>
+<?php include "../includes/footer.php"; ?>
+</body>
+</html>
--- pages/supply_add.php
+++ pages/supply_add.php
@@ -12,6 +12,7 @@ require "../config/db.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Заказы поставщика</title>
+    <link rel='stylesheet' href='../css/style.css'/>
 </head>
 <body>
 <?php  include "../includes/header.php"; ?>
